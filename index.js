79d31f7623a790170616233f80ae48635d59cc64
techforword <112968279+techforword@users.noreply.github.com>
2022-09-06 20:48:11 +0300

    add new get req

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-import express from "express";
+import express from 'express';
 import fetch from 'node-fetch';
 
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.get('/api/v1/courses', async (req, res) => {
-    const options = {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            apiKey: 'cXycM0ts1IRN7293bbH1M8F0NbhYRBsx',
-        },
-    };
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      apiKey: 'cXycM0ts1IRN7293bbH1M8F0NbhYRBsx',
+    },
+  };
 
+  const response = await fetch(
+    'https://developers.teachable.com/v1/courses',
+    options
+  );
+  const courses = await response.json();
 
-    const response = await fetch('https://developers.teachable.com/v1/courses', options);
-    const courses = await response.json();
+  return res.json(courses);
+});
 
+app.get('/api/v1/courses/1335944/enrollments', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      apiKey: 'cXycM0ts1IRN7293bbH1M8F0NbhYRBsx',
+    },
+  };
 
+  const response = await fetch(
+    'https://developers.teachable.com/v1/courses/1335944/enrollments',
+    options
+  );
+  const courses = await response.json();
 
-
-
-    return res.json(courses)
-
-})
+  return res.json(courses);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
