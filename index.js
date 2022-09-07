fdd3669acaf6af5d749283596aa963d82f175ebf
techforword <112968279+techforword@users.noreply.github.com>
2022-09-07 11:36:02 +0300

    .

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const cors = require('cors');
 
-var corsOptions = {
-  origin: 'http://127.0.0.1:5500/',
-  optionsSuccessStatus: 200,
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.get('/api/v1/courses', cors(corsOptions), async (req, res) => {
